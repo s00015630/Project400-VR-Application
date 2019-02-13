68eaf043e1284f3499e776a5c6d2690dfea57122
mark o shea <s00015630@mail.itsligo.ie>
2019-02-13 10:05:54 +0000

    Added large Crowd. Added random questions

---
+++ VrPresentationApp4/Assets/Scripts/AskRandomQuestions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AskRandomQuestions : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip[] audioClipArray;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+    // Start is called before the first frame update
+    public void GetRandomQuestion()
+    {
+        audioSource.clip = audioClipArray[Random.Range(0, audioClipArray.Length)];
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+
+    
+}
