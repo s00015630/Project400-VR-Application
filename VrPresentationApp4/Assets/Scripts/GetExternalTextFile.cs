78b5ec9413c7fe93d383cdc8a94191df3b2df945
mark o shea <s00015630@mail.itsligo.ie>
2019-03-01 22:34:07 +0000

    Get notes from web

--- VrPresentationApp4/Assets/Scripts/GetExternalTextFile.cs
+++ VrPresentationApp4/Assets/Scripts/GetExternalTextFile.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.IO;
+using System;
 using TMPro;
+using System.Collections;
+using UnityEngine.Networking;
 
 public sealed class GetExternalTextFile : MonoBehaviour
 {
@@ -10,28 +13,58 @@ public sealed class GetExternalTextFile : MonoBehaviour
     {        
         m_Text = GetComponent<TextMeshProUGUI>();
         m_Text.text = "Attempting to load text file";
-        string fileName = "VrNotes.txt";
-        string filePath;
-        filePath = Path.Combine(Application.persistentDataPath, fileName);
+        string fileName = "VrNots.txt";
+        string filePathTesting;
+        
 
-        if (!File.Exists(filePath))
+        filePathTesting = Path.Combine(Application.persistentDataPath, fileName);
+        
+        if (!File.Exists(filePathTesting))
         {
-            m_Text.text = "Text file not found. File must be a text file and named \"VrNotes.txt\"";
-            Debug.LogErrorFormat("Error reading {0}\nFile does not exist!", filePath);
+            GetTextWeb();
+            Debug.Log("Attempted to get from website");
+            //m_Text.text = "Text file not found. File must be a text file and named \"VrNotes.txt\"";
+            //Debug.LogErrorFormat("Error reading {0}\nFile does not exist!", filePathTesting);
         }
         else
         {
            
-            StreamReader reader = new StreamReader(filePath);
+            StreamReader reader = new StreamReader(filePathTesting);
             m_Text.text = reader.ReadToEnd();
             Debug.Log(reader.ReadToEnd());
             reader.Close();
         }
     }
+    public void GetTextWeb()
+    {
+        StartCoroutine(GetText());
+        
+    }
     public void ButtonClicked()
     {
         Debug.Log("The button is clicked");
     }
+    
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://s3-eu-west-1.amazonaws.com/s00015630bucket1/folder1/vrnotes.txt");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            m_Text.text = "There was an problem retrieving your notes.\nThis could be caused by no internet connection or the server could be down";
+            Debug.Log(www.error);
+        }
+        else
+        {
+            string origin = "NOTES FROM WEB\n";
+            string input = www.downloadHandler.text;
+            m_Text.text = origin+input;
+            Debug.Log(input);
+        }
+    }
+
 
 
 }
--- VrPresentationApp4/Assets/Scripts/GetTextFromWeb.cs
+++ VrPresentationApp4/Assets/Scripts/GetTextFromWeb.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class GetTextFromWeb : MonoBehaviour
 {
+    public TextMeshProUGUI m_Text;
+    
     // Start is called before the first frame update
     void Start()
     {
+        m_Text = GetComponent<TextMeshProUGUI>();
+        m_Text.text = "Attempting to load text file";
         StartCoroutine(GetText());
     }
-
+    
     IEnumerator GetText()
     {
-        UnityWebRequest www = UnityWebRequest.Get("https://docs.google.com/document/d/1EXOGdfRlBaEAFeotCT1i2Kfmcddx-KJ9gliOaIHdyB4/edit");
+        UnityWebRequest www = UnityWebRequest.Get("https://mailitsligo-my.sharepoint.com/personal/s00015630_mail_itsligo_ie/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fs00015630_mail_itsligo_ie%2FDocuments%2FAttachments%2FVrNotes%2Etxt&parent=%2Fpersonal%2Fs00015630_mail_itsligo_ie%2FDocuments%2FAttachments");
         yield return www.SendWebRequest();
 
         if (www.isNetworkError || www.isHttpError)
@@ -23,41 +28,8 @@ public class GetTextFromWeb : MonoBehaviour
         else
         {
             string input = www.downloadHandler.text;
-            string search = "<span class=\"kix-wordhtmlgenerator-word-node\"style=\"font-size:14.666666666666666px;font-family:Arial;color:#000000;background-color:transparent;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;\">";
-            
-            int p = input.IndexOf(search);
-            Debug.Log(www.downloadHandler.text);
-            if (p >= 0)
-            {
-                // move forward to the value
-                int start = p + search.Length;
-                // now find the end by searching for the next closing tag starting at the start position, 
-                // limiting the forward search to the max value length
-                int end = input.IndexOf("<span class=\"goog-inline-block\"style=\"width:7.859375px;height:17.599999999999998px\">&nbsp;</span></span>\",start)");
-                if (end >= 0)
-                {
-                    // pull out the substring
-                    string value = input.Substring(start, end - start);
-                    // finally parse into a float
-                    
-                    Debug.Log("Value = " + value);
-                }
-                else
-                {
-                    Debug.Log("Bad html - closing tag not found");
-                }
-            }
-            else
-            {
-                Debug.Log("Search tag not found!!");
-            }
-
-            //// Show results as text
-            //Debug.Log(www.downloadHandler.text);
-
-            //// Or retrieve results as binary data
-            //byte[] results = www.downloadHandler.data;
-            
+            m_Text.text = input;
+            Debug.Log(input);           
         }
     }
  
